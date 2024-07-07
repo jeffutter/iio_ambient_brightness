8504442d059be43d25e689a5f52f7445884362b8
Jeffery Utter <jeff@jeffutter.com>
2024-07-07 08:26:52 -0500

    Tweak thresholds

--- src/kbd_brightness.rs
+++ src/kbd_brightness.rs
@@ -32,8 +32,10 @@ impl<'a> KBDBrightness<'a> {
 
     pub(crate) fn adjust(&self, new_val: u32) -> Result<()> {
         let new_level = match new_val {
-            v if v < 50 => 3,
-            v if v < 60 => 2,
+            v if v < 55 => 1,
+            v if v < 65 => 2,
+            v if v < 70 => 3,
+            v if v < 75 => 2,
             v if v < 80 => 1,
             _ => 0,
         };
@@ -41,7 +43,7 @@ impl<'a> KBDBrightness<'a> {
         let cur_brightness = self.read()?;
 
         debug!(
-            "KBD: nv:{:?}, nl:{:?}, cb:{:?}",
+            "KBD: value:{:?}, level:{:?}, current:{:?}",
             new_val, new_level, cur_brightness
         );
         if cur_brightness != new_level {
--- src/screen_brightness.rs
+++ src/screen_brightness.rs
@@ -38,25 +38,37 @@ impl<'a> ScreenBrightness<'a> {
     }
 
     fn pct_to_brightness(&self, pct: u32) -> u32 {
-        (pct * (self.max_brightness)) / 100
+        if pct == 0 {
+            return 0;
+        }
+
+        let res = (pct * (self.max_brightness)) / 100;
+
+        if res < 1 {
+            return 1;
+        }
+
+        res
     }
 
     pub(crate) fn adjust(&self, new_val: u32) -> Result<()> {
         let new_pct: u32 = match new_val {
-            v if v < 1 => 5,
-            v if v < 10 => 10,
-            v if v < 20 => 15,
-            v if v < 30 => 20,
-            v if v < 40 => 25,
-            v if v < 50 => 30,
-            v if v < 60 => 35,
-            v if v < 70 => 40,
-            v if v < 80 => 45,
+            v if v < 1 => 2,
+            v if v < 5 => 4,
+            v if v < 10 => 6,
+            v if v < 20 => 7,
+            v if v < 30 => 8,
+            v if v < 40 => 9,
+            v if v < 50 => 10,
+            v if v < 60 => 20,
+            v if v < 70 => 35,
+            v if v < 80 => 40,
             _ => 50,
         };
 
         let offset_new_pct = match self.offset {
-            0..=i8::MAX => new_pct.saturating_add(self.offset.unsigned_abs() as u32),
+            0 => new_pct,
+            1..=i8::MAX => new_pct.saturating_add(self.offset.unsigned_abs() as u32),
             i8::MIN..=-1 => new_pct.saturating_sub(self.offset.unsigned_abs() as u32),
         };
 
@@ -67,7 +79,7 @@ impl<'a> ScreenBrightness<'a> {
         let cur_brightness = self.read()?;
 
         debug!(
-            "Backlight: nv:{:?}, np:{:?}, onp:{:?}, nl:{:?}, cb:{:?}",
+            "Backlight: value:{:?}, percent:{:?}, offset_percent:{:?}, new_level:{:?}, current:{:?}",
             new_val, new_pct, offset_new_pct, new_level, cur_brightness
         );
         if cur_brightness != new_level {
